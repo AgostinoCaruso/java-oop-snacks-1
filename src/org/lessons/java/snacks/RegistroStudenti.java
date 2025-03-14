af404ab0b5dd635c2470ca02b5b2d3d2be5b54e9
AgostinoCaruso <agostinocaruso321@gmail.com>
2025-03-14 11:58:23 +0100

    add get&set

--- src/org/lessons/java/snacks/RegistroStudenti.java
+++ src/org/lessons/java/snacks/RegistroStudenti.java
@@ -7,5 +7,30 @@ package org.lessons.java.snacks;
  Aggiungi un metodo che stampi la lista degli studenti.
  */
 public class RegistroStudenti {
+    //campi
+    private String nomeStudente;
+    private String cognomeStudente;
     
+    RegistroStudenti[] arrayRegistroStudenti = new RegistroStudenti[0];
+    //costruttore
+    public RegistroStudenti(){
+        
+    }
+
+    //get&set
+    public String getNomeStudente(){
+        return nomeStudente;
+    }
+    public void setNomeStudente(String nome){
+        this.nomeStudente = nome;
+    }
+
+    public String getCognomeStudente(){
+        return cognomeStudente;
+    }
+    public void setCognomeStudente(String cognome){
+        this.cognomeStudente = cognome;
+    }
+    //metodi
+
 }
