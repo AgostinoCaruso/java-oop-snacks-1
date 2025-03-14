d8bc02d19eb33bc25a684b1ee7e24171ca13adee
AgostinoCaruso <agostinocaruso321@gmail.com>
2025-03-14 11:20:31 +0100

    fix

--- src/org/lessons/java/snacks/Studente.java
+++ src/org/lessons/java/snacks/Studente.java
@@ -1,5 +1,10 @@
 package org.lessons.java.snacks;
-
+/*
+ Snack 1: Studente
+Scrivi una classe Studente con i seguenti attributi: nome, cognome, età. 
+Implementa un costruttore che accetta tre parametri per inizializzare questi attributi. 
+Aggiungi un metodo che restituisca una stringa concatenata dei valori (es. Anselmo Bitta, 24 anni).
+ */
 public class Studente {
     //campi
     private String nome;
