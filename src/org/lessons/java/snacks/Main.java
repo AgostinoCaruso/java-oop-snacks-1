fc142f8b521ecfded1bcf4a4b54195f1be385dc8
AgostinoCaruso <agostinocaruso321@gmail.com>
2025-03-14 12:39:50 +0100

    Registro studenti

--- src/org/lessons/java/snacks/Main.java
+++ src/org/lessons/java/snacks/Main.java
@@ -4,12 +4,12 @@ import java.math.BigDecimal;
 
 public class Main {
     public static void main(String[] args) {
+        //Snack-1
         // Studente studente1 = new Studente("Giacomo","Rossi",20);
-
         // System.out.println(studente1.nomeConcatenato());
 
+        //Snack-2
         // ContoBancario conto1 = new ContoBancario("000000123456");
-
         // System.out.println(conto1.getSaldo());
         // BigDecimal sommaDepositata = new BigDecimal("999.99");
         // conto1.depositaSaldo(sommaDepositata);
@@ -17,5 +17,19 @@ public class Main {
         // BigDecimal sommaPrelevata = new BigDecimal("300.99");
         // conto1.prelevaSaldo(sommaPrelevata);
         // System.out.println(conto1.getSaldo());
+
+        //Snack-3
+        RegistroStudenti studente1 = new RegistroStudenti("Fra", "Totti");
+        System.out.println(RegistroStudenti.arrayRegistroStudenti.length);
+        studente1.setNewRegistroStudentiArray();
+        System.out.println(RegistroStudenti.arrayRegistroStudenti.length);
+        RegistroStudenti studente2 = new RegistroStudenti("Dua", "Lipa");
+        studente2.setNewRegistroStudentiArray();
+        RegistroStudenti studente3 = new RegistroStudenti("Valeria","Rossi");
+        studente3.setNewRegistroStudentiArray();
+        RegistroStudenti studente4 = new RegistroStudenti("Kam", "Polaris");
+        studente4.setNewRegistroStudentiArray();
+        System.out.println(RegistroStudenti.arrayRegistroStudenti.length);
+        System.out.println(RegistroStudenti.stampaStudenti());
     }
 }
--- src/org/lessons/java/snacks/RegistroStudenti.java
+++ src/org/lessons/java/snacks/RegistroStudenti.java
@@ -1,4 +1,5 @@
 package org.lessons.java.snacks;
+
 /*
  Crea una classe RegistroStudenti che rappresenti un registro di studenti. 
  Utilizza un array privato per memorizzare oggetti Studente. 
@@ -7,30 +8,56 @@ package org.lessons.java.snacks;
  Aggiungi un metodo che stampi la lista degli studenti.
  */
 public class RegistroStudenti {
-    //campi
+    // campi
+    public static RegistroStudenti[] arrayRegistroStudenti = new RegistroStudenti[0];
     private String nomeStudente;
     private String cognomeStudente;
-    
-    RegistroStudenti[] arrayRegistroStudenti = new RegistroStudenti[0];
-    //costruttore
-    public RegistroStudenti(){
-        
+
+    // costruttore
+    public RegistroStudenti(String nome, String cognome) {
+        this.nomeStudente = nome;
+        this.cognomeStudente = cognome;
     }
 
-    //get&set
-    public String getNomeStudente(){
+    // get&set
+    public String getNomeStudente() {
         return nomeStudente;
     }
-    public void setNomeStudente(String nome){
+
+    public void setNomeStudente(String nome) {
         this.nomeStudente = nome;
     }
 
-    public String getCognomeStudente(){
+    public String getCognomeStudente() {
         return cognomeStudente;
     }
-    public void setCognomeStudente(String cognome){
+
+    public void setCognomeStudente(String cognome) {
         this.cognomeStudente = cognome;
     }
-    //metodi
 
+    // metodi
+    public void setNewRegistroStudentiArray() {
+        
+        RegistroStudenti[] temp = arrayRegistroStudenti;
+        int num = arrayRegistroStudenti.length;
+
+        arrayRegistroStudenti = new RegistroStudenti[num + 1];
+
+        arrayRegistroStudenti[0] = this;
+
+        // for (int i = 1; i < arrayRegistroStudenti.length; i++) {
+        //     arrayRegistroStudenti[i] = temp[i - 1];
+        // }
+        System.arraycopy(temp, 0, arrayRegistroStudenti, 1, num);
+    }
+
+    public static String stampaStudenti() {
+        String accumulo = "";
+        for (int i = 0; i < arrayRegistroStudenti.length; i++) {
+            accumulo += arrayRegistroStudenti[i].nomeStudente + "-"
+                    + arrayRegistroStudenti[i].cognomeStudente + "\n";
+        }
+        return accumulo;
+    }
 }
